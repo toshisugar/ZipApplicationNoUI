c55ad8fdcf1aae4ea2e56458f3073a7ec603be50
toshisugar <39898922+toshisugar@users.noreply.github.com>
2018-11-10 03:09:47 +0900

    ファイル名入力・保存先選択ダイアログメソッド実装。

--- ZipApplicationNoUI/Program.cs
+++ ZipApplicationNoUI/Program.cs
@@ -18,12 +18,14 @@ namespace ZipApplicationNoUI
         {
             InitializeComponent();
             SelectFileDialog();
-            
+            SelectFolderDialog();
+            zip();
         }
 
         //
         static string directoryPath;
         static string files;
+        static string saveFilePath;
 
         //前回見ていたパスを記憶するメソッド
         private static void InitializeComponent()
@@ -42,42 +44,61 @@ namespace ZipApplicationNoUI
         //ファイル選択ダイアログ生成メソッド
         private static void SelectFileDialog()
         {
-            var fileContent = string.Empty;
-            var filePath = string.Empty;
+            //var fileContent = string.Empty;
+            //var filePath = string.Empty;
 
             OpenFileDialog openFileDialog = new OpenFileDialog();
             {
                 openFileDialog.InitialDirectory = directoryPath;
                 openFileDialog.RestoreDirectory = true;
-                openFileDialog.Title = "Zip化したいファイルを選択してください(複数可)。";
+                openFileDialog.Title = "ZIP化したいファイルを選択し、「OK」を押してください(複数可)。";
                 openFileDialog.Multiselect = true;
-
 
                 //ファイルダイアログのOKボタンが押されたときの処理
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
+                    Console.WriteLine("選択ファイルパス：");
                     foreach (string files in openFileDialog.FileNames)
                     {
-                    Console.WriteLine(files);
+                        Console.WriteLine(files);
                     }
-                    Console.ReadLine();
-                //    //Get the path of specified file
-                //    filePath = directoryPath = openFileDialog.InitialDirectory;
-                //    Settings.Default.SourcePath = directoryPath = openFileDialog.FileName;
-                //    Settings.Default.Save();
 
-                //    //Read the contents of the file into a stream
-                //    var fileStream = openFileDialog.OpenFile();
+                    //    //Get the path of specified file
+                    //    filePath = directoryPath = openFileDialog.InitialDirectory;
+                    //    Settings.Default.SourcePath = directoryPath = openFileDialog.FileName;
+                    //    Settings.Default.Save();
 
-                //    using (StreamReader reader = new StreamReader(fileStream))
-                //    {
-                //        fileContent = reader.ReadToEnd();
-                //    }
-                //}
-                //else
-                //{
-                //    //★★★キャンセルや×ボタンが押されたときの処理★★★
+                    //    //Read the contents of the file into a stream
+                    //    var fileStream = openFileDialog.OpenFile();
+
+                    //    using (StreamReader reader = new StreamReader(fileStream))
+                    //    {
+                    //        fileContent = reader.ReadToEnd();
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    //★★★キャンセルや×ボタンが押されたときの処理★★★
                 }
+            }
+        }
+
+        //ファイル名入力及び保存先フォルダ選択ダイアログ生成メソッド
+        private static void SelectFolderDialog()
+        {
+            string sourceDirectory = directoryPath;                        //圧縮するファイルが入っているフォルダのパスをsouceDirectoryに格納
+            var saveFileDialog = new SaveFileDialog            //圧縮したファイルを保存するフォルダを指定するダイアログボックスの生成
+            {
+                Title = "ZIPファイル名を入力して、保存先を指定し、「OK」を押してください。",
+                InitialDirectory = Path.GetDirectoryName(sourceDirectory),
+            };
+
+            //if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                saveFilePath = saveFileDialog.FileName; 
+                Console.WriteLine("\r\nZIPファイル名：\r\n" + Path.GetFileName(saveFileDialog.FileName) + ".zip\r\n");
+                Console.WriteLine("保存先：\r\n" + saveFileDialog.FileName);
             }
         }
 
@@ -86,28 +107,22 @@ namespace ZipApplicationNoUI
         {
             string password = System.Web.Security.Membership.GeneratePassword(8, 0);                //パスワード自動生成
 
-            var zip = ZipFile.Create(files);
+            var zip = ZipFile.Create(saveFilePath);
             zip.BeginUpdate();
             zip.Password = password;                                                                //自動生成したパスワードをZipファイルに設定
-            
-            foreach (var filePath in "★★★ファイルパスが格納されているリスト名を入力してね！★★★")
+
+            foreach (var filePath in files)
             {
                 Add(zip, @"filePath");
             }
             zip.CommitUpdate();
         }
-        
+
         //Zip圧縮の際にファイルパスからファイル名を抽出するメソッド
         private static void Add(ZipFile zip, string path)
         {
             var name = Path.GetFileName(path);          //path(指定したパス文字列)のファイル名と拡張子を、nameに代入
             zip.Add(path, name);                        //zipメソッドで作成された圧縮後のファイル名に、パスとファイル名をAddする
         }
-
-
-
-
-
-
     }
 }
