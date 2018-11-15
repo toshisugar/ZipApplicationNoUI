850a73a5e03c93a7f10c38b45ea94cd40635c60d
toshisugar <39898922+toshisugar@users.noreply.github.com>
2018-11-15 17:35:20 +0900

    ファイル名とパスワードをメモ帳に張り付けて、それを開くメソッド実装

--- ZipApplicationNoUI/Program.cs
+++ ZipApplicationNoUI/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Win32;
 using System.Windows.Forms;
 using ZipApplicationNoUI.Properties;
+using System.Diagnostics;
 
 namespace ZipApplicationNoUI
 {
@@ -16,17 +17,20 @@ namespace ZipApplicationNoUI
         [STAThread]
         static void Main(string[] args)
         {
-            InitializeComponent();      //ファイアログボックスで前回選択したディレクトリを記憶するメソッド
+            //InitializeComponent();      //ファイアログボックスで前回選択したディレクトリを記憶するメソッド
             SelectFileDialog();         //Zip圧縮するファイル選択ダイアログメソッド
             SelectFolderDialog();       //生成したZipファイルの保存先選択ダイアログメソッド
             Zip();                      //Zip化するメソッド
-            Console.ReadLine();         //コンソール画面を残す
+            CreateText();
+            //Console.ReadLine();         //処理が終了してもコンソール画面を残す
         }
 
         //
         static string directoryPath;
         static List<string> filePath = new List<string>();
         static string saveFilePath;
+        static string textFileName;
+        static string password;
 
         //前回見ていたパスを記憶するメソッド
         private static void InitializeComponent()
@@ -100,26 +104,30 @@ namespace ZipApplicationNoUI
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
                 saveFilePath = saveFileDialog.FileName;
-                Console.WriteLine("\r\nZIPファイル名：\r\n" + Path.GetFileName(saveFileDialog.FileName) + ".zip\r\n");
+                Console.WriteLine("\r\nZIPファイル名：\r\n" + Path.GetFileName(saveFileDialog.FileName) + "\r\n");
                 Console.WriteLine("保存先：\r\n" + saveFileDialog.FileName);
+                textFileName = saveFileDialog.FileName;
             }
         }
 
         //Zip圧縮メソッド
         private static void Zip()
         {
-            string password = System.Web.Security.Membership.GeneratePassword(8, 0);                //パスワード自動生成
+            password = System.Web.Security.Membership.GeneratePassword(8, 0);                //パスワード自動生成
 
             var zip = ZipFile.Create(saveFilePath);
 
             zip.BeginUpdate();
-            zip.Password = password;                                                                //自動生成したパスワードをZipファイルに設定
+                                                                //自動生成したパスワードをZipファイルに設定
 
             foreach (var i in filePath)
             {
                 Add(zip, i);
             }
+            zip.Password = password;
             zip.CommitUpdate();
+
+            Console.WriteLine("パスワード：\r\n" + password);
         }
 
         //Zip圧縮の際にファイルパスからファイル名を抽出するメソッド
@@ -128,5 +136,17 @@ namespace ZipApplicationNoUI
             var name = Path.GetFileName(path);          //path(指定したパス文字列)のファイル名と拡張子を、nameに代入
             zip.Add(path, name);                        //zipメソッドで作成された圧縮後のファイル名に、パスとファイル名をAddする
         }
+
+        //ファイル名とパスワードをメモ帳に入力するメソッド
+        private static void CreateText()
+        {
+            //第1引数：ファイルパス
+            //第2引数：追記するテキスト 
+            var zipFileName = Path.GetFileName(textFileName);
+            File.WriteAllText(textFileName + ".txt", "添付ファイル名：" + zipFileName + "\r\n" + "パスワード：" + password);
+            Process.Start(textFileName + ".txt");
+        }
+
+        //ファイル名とパスワードが入力されたメモ帳を開くメソッド
     }
 }
